00b3395e5ef2489859e84105c17a8de254e4409d
Angel Rodriguez Herrera <angelh@intedilab.com>
2019-03-26 15:19:28 -0600

    updated formulario

--- formulario/fechaYhora.js
+++ formulario/fechaYhora.js
@@ -1,4 +1,5 @@
-var d = new Date(); 
-document.write(d.getDate() + "/" + (d.getMonth() +1) + "/" + d.getFullYear(), ' --- ' +d.getHours(),':'+d.getMinutes(),':'+d.getSeconds()); 
+var hoy = new Date(); 
+document.write(hoy.getDate() + "/" + (hoy.getMonth() +1) + "/" + hoy.getFullYear(), ' --- ' +hoy.getHours(),':'+hoy.getMinutes(),':'+hoy.getSeconds()); 
+
 
 
