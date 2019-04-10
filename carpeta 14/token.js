357e387b88774cfc7df33953516872a730b42a14
Angel Rodriguez Herrera <42501960+M1st3rAng3l@users.noreply.github.com>
2019-04-10 13:34:51 -0500

    token

---
+++ carpeta 14/token.js
@@ -0,0 +1,38 @@
+function addToken(bs, user, token){
+    let now = newDate();
+    return{...bd[user]
+    
+    }
+
+}
+
+    function go(){
+        let now = newDate();
+        let bd={
+            user1={
+                token=[
+                    {
+                        timestamp: now.getTime(),
+                        token:100
+                    },
+                    {
+                        timestamp: now.getTime(),
+                        token: 101
+                    }
+                ]
+            },
+            user2={
+                token=[
+                    {
+                        timestamp: now.getTime(),
+                        token:100
+                    },
+                    {
+                        timestamp: now.getTime(),
+                        token: 101
+                    }
+                ]
+            },
+        };
+        console.log(addToken(bd, "user2", 333));
+    }
