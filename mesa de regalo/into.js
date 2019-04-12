f3ffe0e930ce4ce4aff7be4f91a942fa9617791c
Angel Rodriguez Herrera <angelh@intedilab.com>
2019-04-12 16:10:30 -0500

    ajustes

---
+++ mesa de regalo/into.js
@@ -0,0 +1,15 @@
+function saludo(){
+    alert('¡¡Bienvenidos!!');
+}
+
+function despedida(){
+    alert('¡¡Gracias por su visita!!');
+}
+
+var red[];
+var blue[];
+var green[];
+var yellow[];
+
+listColors= [];
+
